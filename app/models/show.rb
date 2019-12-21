01cb2e2a81fb5a03b75498d648e57e799644a2fa
Kevin Downs <kevin.downs04@gmail.com>
2019-12-21 00:15:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,11 +2,11 @@
 class Show < ActiveRecord::Base 
   
   def self.highest_rating
-    self.maximum(:number_of_stars)
+    self.maximum(:rating)
   end
   
   def self.most_popular_show
-    show = self.find_by(:number_of_stars, self.highest_rating)
+    show = self.find_by(:rating, self.highest_rating)
   end
   
 end
